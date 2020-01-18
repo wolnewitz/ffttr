efbf6c21feb30877f3e6e51e9bb85bf205d3204e
Cory Wolnewitz <wolnewitz@gmail.com>
2020-01-18 15:17:43 -0500

    Delete bunch of shit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,15 @@
 import React from "react"
-import { Link } from "gatsby"
 
-import Layout from "../components/layout"
-import Image from "../components/image"
+import Navigation from "../components/Navigation"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
-  <Layout>
+  <React.Fragment>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <div className="home-header">
+      <Navigation />
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
+  </React.Fragment>
 )
 
 export default IndexPage
