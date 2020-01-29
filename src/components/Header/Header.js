d7af968ee88bfa8c06820e3baf7a772d8cc4135f
Cory Wolnewitz <wolnewitz@gmail.com>
2020-01-29 16:55:14 -0500

    More mobile fixes

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,32 +5,32 @@ import Navigation from "../Navigation"
 
 const Header = ({ navData, heroData }) => {
   return (
-    <div className={styles.heroImage}>
-      <Navigation data={navData} />
-      <div className={styles.heroTextWrapper}>
-        <div className={styles.heroBannerText}>{heroData.headerText}</div>
-        <div className={styles.heroInfo}>
-          {heroData.headerDescription.headerDescription}
-        </div>
-        <a href={heroData.headerLink.linkUrl} className={styles.heroButton}>
-          {heroData.headerLink.linkText}
-        </a>
-      </div>
-      <div>
-        <div className={styles.bottomHeroWrapper}>
-          <div className={styles.bottomHeroTextBlocks}>
-            {heroData.bottomTextBlocks.map(({ heading, description }) => (
-              <div key={heading} className={styles.bottomTextWrapper}>
-                <div className={styles.bottomTextHeading}>{heading}</div>
-                <div className={styles.bottomTextDescription}>
-                  {description.description}
-                </div>
-              </div>
-            ))}
+    <>
+      <div className={styles.heroImage}>
+        <Navigation data={navData} />
+        <div className={styles.heroTextWrapper}>
+          <div className={styles.heroBannerText}>{heroData.headerText}</div>
+          <div className={styles.heroInfo}>
+            {heroData.headerDescription.headerDescription}
           </div>
+          <a href={heroData.headerLink.linkUrl} className={styles.heroButton}>
+            {heroData.headerLink.linkText}
+          </a>
         </div>
       </div>
-    </div>
+      <div className={styles.bottomHeroWrapper}>
+        <div className={styles.bottomHeroTextBlocks}>
+          {heroData.bottomTextBlocks.map(({ heading, description }) => (
+            <div key={heading} className={styles.bottomTextWrapper}>
+              <div className={styles.bottomTextHeading}>{heading}</div>
+              <div className={styles.bottomTextDescription}>
+                {description.description}
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
   )
 }
 export default Header
